f090b91fc0551cb43f20dc5e5d34a5b9e98a3d0c
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-05-16 16:44:50 -0400

    check it updates local stock status after being tracked

--- tests/Unit/StockTest.php
+++ tests/Unit/StockTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Unit;
 
+use App\Clients\Client;
 use App\Clients\ClientException;
+use App\Clients\StockStatus;
 use App\Models\Retailer;
 use App\Models\Stock;
 use Database\Seeders\RetailerWithProductSeeder;
+use Facades\App\Clients\ClientFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -23,5 +26,20 @@ class StockTest extends TestCase
         $this->expectException(ClientException::class);
 
         Stock::first()->track();
+    }
+
+    /** @test */
+    function it_updates_local_stock_status_after_being_tracked()
+    {
+        $this->seed(RetailerWithProductSeeder::class);
+
+        ClientFactory::shouldReceive('make->checkAvailability')->andReturn(
+            new StockStatus($available = true, $price = 9900)
+        );
+
+        $stock = tap(Stock::first())->track();
+
+        $this->assertTrue($stock->in_stock);
+        $this->assertEquals(9900, $stock->price);
     }
 }
